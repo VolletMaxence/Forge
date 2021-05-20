129d85e037814310f565145c9788112ddf1c70a7
VOLLETMaxence <mvollet.la-providence.net>
2021-05-20 08:45:14 +0200

    Ignore paramètre BDD

--- bdd.php
+++ bdd.php
@@ -1,7 +1,7 @@
 <?php
 
     $user = 'root';
-    $pass = '';
-    $bdd = new PDO('mysql:host=localhost; dbname=fs', $user, $pass);
+    $pass = 'root';
+    $bdd = new PDO('mysql:host=192.168.64.116; dbname=fs', $user, $pass);
 
 ?>
